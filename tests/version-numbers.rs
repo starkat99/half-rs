bfa963dce401ca2e5a3b67fb0a0da3a8d8f663f4
Kathryn Long <squeeself@gmail.com>
2020-05-09 22:57:40 -0400

    fix CI for Rust 1.32

--- tests/version-numbers.rs
+++ tests/version-numbers.rs
@@ -7,3 +7,8 @@ fn test_readme_deps() {
 fn test_html_root_url() {
     version_sync::assert_html_root_url_updated!("src/lib.rs");
 }
+
+#[test]
+fn test_changelog_version() {
+    version_sync::assert_contains_regex!("CHANGELOG.md", "^## \\[{version}\\]");
+}
